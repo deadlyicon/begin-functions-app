62d560f307a5cd7ad3e78c21e8b612c0eb052cd9
Jared Grippe <deadlyicon@users.noreply.github.com>
2018-09-28 23:36:50 -0700

    status page

--- src/html/get-status/index.js
+++ src/html/get-status/index.js
@@ -6,19 +6,17 @@ let defaultHTML = `
 <html lang=en>
   <head>
     <meta charset=utf-8>
-    <title>Hi!</title>
+    <title>Status</title>
   </head>
   <body style="font-family: sans-serif;">
-    <h1>Hello world!</h1>
+    <h1>Status</h1>
   </body>
 </html>
 `
 
-function route (req, res) {
+exports.handler = begin.html.get((req, res) => {
   console.log(JSON.stringify(req, null, 2))
   res({
     html: defaultHTML
   })
-}
-
-exports.handler = begin.html.get(route)
+})
